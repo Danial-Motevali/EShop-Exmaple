020d60d2f6b557f2137ce25e65e83e54d174e1b3
Danial Motevali <danial.motevali.82@gmail.com>
2023-10-19 20:28:40 +0330

    add AppService interface

---
+++ App.Domain.Core/Shop/Contacts/AppService/ICategoryAppService.cs
@@ -0,0 +1,22 @@
+﻿using App.Domain.Core.Shop.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.Shop.Contacts.AppService
+{
+    public interface ICategoryAppService
+    {
+        Task<int> Add(CategoryInputDto productInput, CancellationToken cancellationToken);
+
+        Task<int> Update(int id, CategoryInputDto productInput, CancellationToken cancellationToken);
+
+        Task<bool> Delete(int id, CancellationToken cancellationToken);
+
+        Task<CategoryOutputDto> GetById(int id, CancellationToken cancellationToken);
+
+        Task<List<CategoryOutputDto>> GetAll(CancellationToken cancellationToken);
+    }
+}
